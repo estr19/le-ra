14c888e9992c0b4f4646b9a66c9228d244a3a32c
Elena Strakhova <elenastrakhova@gmail.com>
2025-06-14 23:10:01 -0400

    borders and gradients

--- src/App.js
+++ src/App.js
@@ -11,29 +11,45 @@ function App() {
           return (
             <div className="book" key={id}>
               <div className="blogEntry">
-                <a href={link} target="_blank" rel="noopener noreferrer">
-                <figure>
-                  <img src={cover} alt={name}/>
-                </figure>
-                <h2>
-                    "{name}"
-                </h2>
-                <h3 className="author">{author}</h3>
-                <div
-                  className="Stars"
-                  style={{ "--rating": `${bookRating}` }}
-                  title={
-                    bookRating === "0"
-                      ? "Not yet rated"
-                      : `${bookRating} out of 5`
-                  }
-                />
-                </a>
-                <div className='description'>
-                  <div className='dItems'><p><b>@bensmom33:</b> {textB}</p></div>
-                  <div className='dItems'><p><b>@estr19:</b> {textE}</p></div>
-                  <div className='dItems'><p><b>Tropes:</b> {tropes}</p></div>
-                  <div className='dItems'><p><b>Additional resources:</b> {addOns}</p></div>
+                <div className="summary">
+                  <figure>
+                    <img src={cover} alt={name} />
+                  </figure>
+                  <a href={link} target="_blank" rel="noopener noreferrer">
+                    <h2>"{name}"</h2>
+                    <h3 className="author">{author}</h3>
+                  </a>
+                  <div
+                    className="Stars"
+                    style={{ "--rating": `${bookRating}` }}
+                    title={
+                      bookRating === "0"
+                        ? "Not yet rated"
+                        : `${bookRating} out of 5`
+                    }
+                  />
+                </div>
+                <div className="description">
+                  <div className="dItems">
+                    <p>
+                      <b>@bensmom33:</b> {textB}
+                    </p>
+                  </div>
+                  <div className="dItems">
+                    <p>
+                      <b>@estr19:</b> {textE}
+                    </p>
+                  </div>
+                  <div className="dItems">
+                    <p>
+                      <b>Tropes:</b> {tropes}
+                    </p>
+                  </div>
+                  <div className="dItems">
+                    <p>
+                      <b>Additional resources:</b> {addOns}
+                    </p>
+                  </div>
                 </div>
               </div>
             </div>
