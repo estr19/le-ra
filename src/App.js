cfd7b92957cfec1a4b2ce5a24d20c0aad1c3b536
Elena Strakhova <elenastrakhova@gmail.com>
2025-06-16 13:19:28 -0400

    size changes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
       <h1>Le-Ra and the Princesses of Publications</h1>
       <div className="container">
         {books.map((book) => {
-          const { id, name, author, bookRating, cover, link, textB, textE, tropes, highlightsE} =
+          const { id, name, author, bookRating, cover, GR, SG, textB, textE, tropes, highlightsE} =
             book;
           return (
             <div className="book" key={id}>
@@ -14,10 +14,9 @@ function App() {
                   <figure>
                     <img src={cover} alt={name} />
                   </figure>
-                  <a href={link} target="_blank" rel="noopener noreferrer">
-                    <h2>"{name}"</h2>
-                    <h3 className="author">{author}</h3>
-                  </a>
+                  <h2>"{name}"</h2>
+                  <h3 className="author">{author}</h3>
+                  <h4><a href={GR} target="_blank" rel="noopener noreferrer">Goodreads</a> | <a href={SG} target="_blank" rel="noopener noreferrer">StoryGraph</a></h4>
                   <div
                     className="Stars"
                     style={{ "--rating": `${bookRating}` }}
@@ -29,17 +28,17 @@ function App() {
                   />
                 </div>
                 <div className="description">
-                  <div className="dItems">
+                  <div className="items">
                     <b>@bensmom33:</b> {textB}
                   </div>
-                  <div className="dItems">
+                  <div className="items">
                     <b>@estr19:</b> {textE}<br></br>
                     <i>My favorite quotes and passages from this book can be found</i> <a href={highlightsE} target="_blank" rel="noopener noreferrer"><b>here</b></a>.
                   </div>
-                  <div className="dItems">
+                  <div className="items">
                     <b>Tropes:</b> {tropes}
                   </div>
-                  {/* <div className="dItems">
+                  {/* <div className="items">
                     <b>Additional resources:</b> {addOns}
                   </div> */}
                 </div>
