63b7b8ba3004aadcd2e0db72e0b405be541162e4
Elena Strakhova <elenastrakhova@gmail.com>
2025-06-18 17:11:40 -0400

    contact us

--- src/ContactUs.js
+++ src/ContactUs.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 
 function ContactUs() {
   const clearInput = () => {
@@ -10,21 +10,18 @@ function ContactUs() {
   }
 
   return (
-    <div>
-      <div className='container'>
-        <span className='opaque' style={{padding: '2em'}}>
-          <h1 className='gradient-text'>Contact Us!</h1>
-        </span>
-      </div>
-      <div className="container">
-        <form id="form" action="https://formspree.io/f/mgedarra" method="POST" className="topBefore opaque">
-          <label className="formLabel"><span className='opaque' style={{textAlign: 'center'}}>NAME</span></label>
-          <input className='opaque' id="name" type="text" name="name" placeholder="your name" required="" />
-          <label className="formLabel"><span className='opaque'>E-MAIL</span></label>
-          <input className='opaque' id="email" type="email" name="_replyto" placeholder="your e-mail" required="" />
-          <label className="formLabel"><span className='opaque'>MESSAGE</span></label>
-          <textarea className='opaque' id="message" type="text" name="message" placeholder="a brief message" required=""></textarea>
-          <input id="submit" type="submit" value="SUBMIT" onClick={() => clearInput()}/>
+    <div className='container'>
+      <h1 className='gradient-text'>Contact Us!</h1>
+      <div className='contactUs'>
+        <h2>Have a question, or a suggestion? Drop a message below!</h2>
+        <form action='https://formspree.io/f/mgedarra' method='POST'>
+          <label><h3>NAME</h3>
+          <input type='text' name='name' placeholder='your name' required='' /></label>
+          <label><h3>E-MAIL</h3>
+          <input type='email' name='_replyto' placeholder='your e-mail' required='' /></label>
+          <label><h3>MESSAGE</h3>
+          <textarea type='text' name='message' placeholder='a brief message' required=''></textarea></label>
+          <input id='submit' type='submit' value='SUBMIT' onClick={() => clearInput()}/>
         </form>
       </div>
     </div>
