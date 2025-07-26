2d81621a4d8157ca2e422f35e42f3b6bb686f586
Elena Strakhova <elenastrakhova@gmail.com>
2025-07-25 22:29:04 -0400

    mara quotes

--- src/rwrbConvo.js
+++ src/rwrbConvo.js
@@ -1,35 +1,132 @@
 import bensmom33pfp from './bensmom33pfp.jpg';
-import estr19pfp from './estr19pfp.jpg';
+// import estr19pfp from './estr19pfp.jpg';
 
 export const rwrbConvo = [
   {
     id: 1,
     name: 'bensmom33',
     pfp: bensmom33pfp,
-    message: 'Hey, how did you discover RWRB?',
+    message: `The first is for amazing foreshadowing: notice he does not say he won't have an international incident. The second for using Rich Girl so that the other side of the album is London Luck & Love. "“Well, you don't have to like him, you just have to put on a happy face and not cause an international incident at his brother's wedding.” “Bug, when do I ever not put on a happy face?” Alex says. He pulls a painfully fake grin, and June looks satisfyingly repulsed."
+    "Outside his window, the wind stirs up over the lawn, rustling the linden trees down in the garden. The record on the turntable in the corner has spun out into fuzzy silence. He rolls off the bed and flips it, resetting the needle, and the second side picks up on “London Luck, & Love.”"`,
   },
   {
     id: 2,
-    name: 'estr19',
-    pfp: estr19pfp,
-    message: 'Very randomly! I found this book on one of the listicles and read it on a whim a couple of weeks later, loved it but then promptly moved on to the next book. What are your favorite moments in the book?',
+    name: 'bensmom33',
+    pfp: bensmom33pfp,
+    message: `Once again I have 2 submissions. The first, following the music theme, the inclusion of Patsy Cline, a line from Crazy - "crazy for thinking my love could hold you"; and the second for launching a thousand fics with these HC. "They settle into their usual places: June, perched on her heels at the record collection, foraging for some Patsy Cline; Nora, cross-legged on the floor, uncorking a bottle of red wine; Alex, sitting upside down with his feet on the back of the couch, trying to figure out what he's going to do next."
+    "“Allergies: dust, Tide laundry detergent, and shutting the fuck up.” “Age of first filibuster: nine, at SeaWorld San Antonio, trying to force an orca wrangler into early retirement for, quote, 'inhumane whale practices.'”"`,
   },
   {
     id: 3,
     name: 'bensmom33',
     pfp: bensmom33pfp,
-    message: `From Chapter 7: "Shut up, shut all the way up, oh my God,” Alex hisses, and if he weren't already hell-bent on destroying Henry's infuriating idiot face with his mouth right now, he would consider doing it with his fist. He's focused on the burst of adrenaline carrying his feet over the antique rug, Henry's tie wrapped around his fist, the flash in Henry's eyes. He reaches the nearest wall, shoves Henry against it, and crushes their mouths together.`,
+    message: `First one, love Alex's description of Raf and which Muddy Waters song was he listening to? Baby, Please Don't Go? "He finds Luna in his office, horn-rimmed reading glasses doing nothing to detract from his usual appearance of a movie star who tripped and fell sideways into politics. Alex has always suspected the soulful brown eyes and perfectly groomed stubble and dramatic cheekbones won back any votes Luna lost by being both Latino and openly gay. The album playing low in the room is an old favorite Alex remembers from Denver: Muddy Waters. When Luna looks up and sees Alex in his doorway, he drops his pen on a haphazard pile of papers and leans back in his chair."
+    Second one is because one day I'm going to write this pre-canon fic."He finds Luna in his office, horn-rimmed reading glasses doing nothing to detract from his usual appearance of a movie star who tripped and fell sideways into politics. Alex has always suspected the soulful brown eyes and perfectly groomed stubble and dramatic cheekbones won back any votes Luna lost by being both Latino and openly gay. The album playing low in the room is an old favorite Alex remembers from Denver: Muddy Waters. When Luna looks up and sees Alex in his doorway, he drops his pen on a haphazard pile of papers and leans back in his chair."`,
   },
   {
     id: 4,
-    name: 'estr19',
-    pfp: estr19pfp,
-    message: `Great excerpt!! I love it and also this one from the same chapter: 'Henry is seated with the prime minister, and Alex sits and chews his food like it's personally wronged him and watches Henry from across the room, seething.' Very Viola Hastings of him 🤭`,
+    name: 'bensmom33',
+    pfp: bensmom33pfp,
+    message: `Obviously a lot happened in this chapter. The first selection surprised me as to the breadth of Henry's pop culture knowledge. "“Alex,” Henry says firmly. “What?” “The turkeys are not going to Jurassic Park you,” he says. “You're not the bloke from Seinfeld. You're Jeff Goldblum. Go to sleep.”"
+    The second was my aww, Henry just gets Alex moment. "He doesn't realize he's been talking for an hour until he finishes retelling what happened at dinner and Henry says, “It sounds like you did your best.” Alex forgets what he was going to say next. He just… Well, he gets told he's great a lot. He just doesn't often get told he's good enough."`,
   },
   {
     id: 5,
     name: 'bensmom33',
     pfp: bensmom33pfp,
-    message: `This quote from Chapter 5 surprised me as to the breadth of Henry's pop culture knowledge: "“Alex,” Henry says firmly. “What?” “The turkeys are not going to Jurassic Park you,” he says. “You're not the bloke from Seinfeld. You're Jeff Goldblum. Go to sleep.”"`,
+    message: `Short one but two postings that launched a thousand fics: the first, Henry's notable shoulder to waist ratio "He thinks about Henry's voice low in his ear over the phone at three in the morning, and suddenly he has a name for what ignites in the pit of his stomach. Henry's hands on him, his thumbs braced against his temples back in the garden, Henry's hands other places, Henry's mouth, what he might do with it if Alex let him. Henry's broad shoulders and long legs and narrow waist, the place his jaw meets his neck and the place his neck meets his shoulder and the tendon that stretches the length between them, and the way it looks when Henry turns his head to shoot him a challenging glare, and his impossibly blue eyes—"
+    and the second, everyone's favorite character to hate on - WASPy Hunter. "Which is how he finds himself out of breath on a Monday afternoon after class, following a staffer who's managed to surpass even him in the caffeination department, on a breakneck tour of the campaign offices. He gets a badge with his name and photo on it, a desk in a shared cubicle, and a WASPy cubicle mate from Boston named Hunter with an extremely punchable face."`,
+  },
+  {
+    id: 6,
+    name: 'bensmom33',
+    pfp: bensmom33pfp,
+    message: `I have 3 selections today. The first two because of iconic lines and the third for reasons... "“Shut up, shut all the way up, oh my God,” Alex hisses, and if he weren't already hell-bent on destroying Henry's infuriating idiot face with his mouth right now, he would consider doing it with his fist. He's focused on the burst of adrenaline carrying his feet over the antique rug, Henry's tie wrapped around his fist, the flash in Henry's eyes. He reaches the nearest wall, shoves Henry against it, and crushes their mouths together."
+    "Turns out being on the receiving end of Henry's royal authority is an extreme fucking turn-on. He thinks, as he's hauled into a bruising kiss, that he'll never forgive himself for it. So, like, fuck the moors."
+    "“Dude, people are writing fan fiction about y'all—” “Yeah, Nora sent me that.” “—I think you can give it a rest.”"`,
+  },
+  {
+    id: 7,
+    name: 'bensmom33',
+    pfp: bensmom33pfp,
+    message: `Narrowed down to 3 selections, the first because it shows that Alex understands and loves every part of Henry, the second because it's a rare example where the movie dialogue improved upon the book and lastly June saying what we all are thinking. "Henry hates those days completely. Alex wishes he could help, but he doesn't particularly mind. He's just as attracted to Henry's cloudy tempers, the way he comes back from them, and the millions of shades in between."
+    "“The official family name is Mountchristen-Windsor,” Henry says. “Hyphenate, like yours. So my full name is… Henry George Edward James Fox-Mountchristen-Windsor.” Alex gapes up at the ceiling. “Oh… my God.” “Truly.” “I thought Alexander Gabriel Claremont-Diaz was bad.”"
+    "June makes a strangled noise, and when he looks back up, she's digging through the top drawer of her dresser. “Oh my God, how are you so smart and so dumb at the same time?” she says, pulling a magazine out from underneath her underwear. He's about to tell her he's not in the mood to look at her tabloids when she throws it at him. An ancient issue of J14, opened to a center page. The photograph of Henry, age thirteen."`,
+  },
+  {
+    id: 8,
+    name: 'bensmom33',
+    pfp: bensmom33pfp,
+    message: `Four this time because they are all amazing and I could have had more so be glad it's just four!
+    "irl chaos demon i'm going for, like, depressed lesbian poet who met a hot yoga instructor at a speakeasy who got her super into meditation and pottery, and now she's starting a new life as a high-powered businesswoman selling her own line of hand-thrown fruit bowls … HRH Prince Dickhead Bitch, you took me there."
+    "“I think I've proven that I can handle royals.” Henry looks doubtful. “You're brave. I could use some of that.”"
+    "“I'll have a gin and tonic, thanks,” it says, and there's Henry in the flesh, sidled up next to him at the bar, looking a little tousled in a soft gray button-down and jeans. Alex wonders for an insane second if his brain has conjured up some kind of stress-induced sex mirage, when Henry says, voice lowered, “You looked rather tragic drinking alone.”"
+    "“You still are. Because you still bloody care so much.” He leans down and presses a kiss into Alex's hair. “And you are good. Most things are awful most of the time, but you're good.”"`,
+  },
+  {
+    id: 9,
+    name: 'bensmom33',
+    pfp: bensmom33pfp,
+    message: `There are so many iconic lines in this chapter but I chose 3 less discussed: 1. Ellen and her PowerPoint slide titles, 2. For the feels, and 3. An underrated Nora line.
+    "The next slide is titled: EXPLORING YOUR SEXUALITY: HEALTHY, BUT DOES IT HAVE TO BE WITH THE PRINCE OF ENGLAND? She apologizes for not having time to come up with better titles. Alex actively wishes for the sweet release of death. The one after is: FEDERAL FUNDING, TRAVEL EXPENSES, BOOTY CALLS, AND YOU."
+    "June plucks away at “Annie's Song,” you fill up my senses like a night in a forest, and the breeze keeps moving to meet the highest branches of the trees, and the water keeps rising to meet the bulkheads, and Henry leans down to meet Alex's mouth, and Alex is. Well, Alex is so in love he could die."
+    "“I didn't realize this was a jazz brunch,” says Nora's voice suddenly, and Henry springs backward so fast he almost puts his ass in the bowl of batter. She sidles up to the forgotten coffeemaker, grinning slyly at them."`,
+  },
+  {
+    id: 10,
+    name: 'bensmom33',
+    pfp: bensmom33pfp,
+    message: `I present to you an anatomy of a scene: rising action, climax, falling action, and resolution.
+    "“I'd rather not do this where we might be overheard,” Henry says, taking a left on the landing. Alex stomps up after him, following him into his bedroom. “Do what?” he says as Henry shuts the door behind them. “What are you gonna do, Henry?” Henry turns to face him at last, and now that Alex's eyes aren't full of rainwater, he can see the skin under his eyes is papery and purple, rimmed pink at his eyelashes. There's a tense set to his shoulders Alex hasn't seen in months, not directed at him at least. “I'm going to let you say what you need to say,” Henry says flatly, “so you can leave.” Alex stares. “What, and then we're over?” Henry doesn't answer him."
+    "“Jesus, could you stop being an obtuse fucking asshole for, like, twenty seconds?” “So glad you flew here to insult me—” “I fucking love you, okay?” Alex half yells, finally, irreversibly. Henry goes very still against the mantelpiece. Alex watches him swallow, watches the muscle that keeps twitching in his jaw, and feels like he might shake out of his skin. “Fuck, I swear. You don't make it fucking easy. But I'm in love with you.” A small click cuts the silence: Henry has taken his signet ring off and set it down on the mantel. He holds his naked hand to his chest, kneading the palm, the flickering light from the fire painting his face in dramatic shadows. “Do you have any idea what that means?”"
+    "“Well,” Alex says. “I do. And you can choose.” “You know bloody well I can't.” “You can try,” Alex tells him, feeling as if it should be the simplest fucking truth in the world. “What do you want?” “I want you—” “Then fucking have me.”"
+    "Alex turns away from him, falling back on his heels like he's been slapped. “Fine,” he finally says. “You know what? Fucking fine. I'll leave.” “Good.” “I'll leave,” he says, and he turns back and leans in, “as soon as you tell me to leave.” “Alex.” He's in Henry's face now. If he's getting his heart broken tonight, he's sure as hell going to make Henry have the guts to do it right. “Tell me you're done with me. I'll get back on the plane. That's it. And you can live here in your tower and be miserable forever, write a whole book of sad fucking poems about it. Whatever. Just say it.” “Fuck you,” Henry says, his voice breaking, and he gets a handful of Alex's shirt collar, and Alex knows he's going to love this stubborn shithead forever."`,
+  },
+  {
+    id: 11,
+    name: 'bensmom33',
+    pfp: bensmom33pfp,
+    message: `I did not realize how short this chapter was but it still has so many memorable things - their first meeting retelling, the list. Here are three of my favorites:
+    "Re: telling Philip, sounds like a great plan. If all else fails, just do what I did and act like a huge jackass until most of your family figures it out on their own."
+    "But the first time I saw you. Rio. I took that down to the gardens. I pressed it into the leaves of a silver maple and recited it to the Waterloo Vase. It didn't fit in any rooms."
+    "God, I want to fight everyone who's ever hurt you, but it was me too, wasn't it? All that time. I'm so sorry."`,
+  },
+  {
+    id: 12,
+    name: 'bensmom33',
+    pfp: bensmom33pfp,
+    message: `I selected the first two because Alex comments on the Solange elevator incident without realizing the irony of June's protection of him, the confession to RAF, and one of my favorite Zahra lines.
+    "“I just mean, like, who is leaking fucking elevator tapes? Who's checking for that? It's not like Solange was in there—”"
+    "He's always known June was one inch from taking a bullet for him, but this? He would never ask her to do this."
+    "And what comes out of Alex's mouth, comes so fast he has no chance to stop it, only divert it out of English at the last second in case it's overheard: “Sería una mentira, porque no sería él.” It would be a lie, because it wouldn't be him."
+    "“Fuck!” he says a third time, spiking the newspaper at the floor. That one was his. It feels obscene to see it there. “How the fuck did they get these?” “Yep,” Zahra agrees. “You dirty did it.”"`,
+  },
+  {
+    id: 13,
+    name: 'bensmom33',
+    pfp: bensmom33pfp,
+    message: `I almost went with the supportive women in this chapter who stepped up to protect their boys: Ellen, June, Zahra, Bea. But instead I went with this trio of quotes.
+    "Alex's laugh comes out half sob, and it's impossible to know if he moves first or if Henry does, but they meet in the middle of the room, Henry's arms around Alex's neck, swallowing him up. If Henry's voice on the phone was a tether, his body is the gravity that makes it possible, his hand gripping the back of Alex's neck a magnetic force, a permanent compass north."
+    "Alex sits, trying for a long moment to absorb it, and says, “I've never … I haven't been through anything like that,” he says, voice rough. “But I've always felt it, in him. There's this side of him that's … unknowable.” He takes a breath. “But the thing is, jumping off cliffs is kinda my thing. That's the choice. I love him, with all that, because of all that. On purpose. I love him on purpose.”"
+    "“For what it's worth,” he says to Philip, “that is the bravest son of a bitch I've ever met.” And he leaves too."`,
+  },
+  {
+    id: 14,
+    name: 'bensmom33',
+    pfp: bensmom33pfp,
+    message: `Happy birthday you wonderful pink book. Selection 1 - June is all of us, Selection 2 - Nora, certified bad ass, and Selection 3 - Ellen, bad ass in chief:
+    "June cries first when she hears about the phone call on the plane, then again at Henry standing up to Philip, and a third time at the crowd outside Buckingham Palace."
+    "“Ellen,” Nora practically yells. The room goes silent, and Nora freezes, realizing. “Uh. Ma'am. Mom-in-law. Please, just. You need to read this.”"
+    "“I'm not giving them shit,” Ellen cuts him off immediately, not even looking up, “especially not after what they did to you. This is real shit. I'm taking this motherfucker down. It has to stick.” She finally puts her highlighter down. “We're leaking it to the press.”"`,
+  },
+  {
+    id: 15,
+    name: 'bensmom33',
+    pfp: bensmom33pfp,
+    message: `I can't believe we are at the end. Here are my final 4: Philip, the redemption tour, Alex and the jacket symbolism, poor Henry learning more about the US, and Alex making sure Henry knows he is part of the family.
+    "Philip walks off, hands in his pockets, and all three of them watch his retreating back until it starts to disappear behind the tall hedges. Bea sighs. “D'you think I should have let him have a go at the cullen skink man for me?” “Not yet,” Henry says. “Give him another six months. He hasn't earned it yet.”"
+    "She emerges with a hanger in her hand: a jacket he's never worn out before, one she convinced him to buy online for an obscene price the night they got drunk and watched The West Wing in a hotel in New York and let the tabloids think they were screwing. It's fucking Gucci, a midnight-blue bomber jacket with red, white, and blue stripes at the waistband and cuffs. “I know it's a lot, but”—she slaps the jacket against his chest—“you give people hope. So, get back out there and be Alex.” He takes the jacket from her and tries it on, checks his reflection in the mirror. It's perfect."
+    "“Come on, you backyard-shooting-range motherfuckers,” Zahra is muttering under her breath beside him when he falls in with his people. “Did she just say backyard shooting range?” Henry asks, leaning into Alex's ear. “Is that a real thing a person can have?” “You really have a lot to learn about America, mijo,” Oscar tells him, not unkindly."
+    "He's so caught up that he doesn't realize at first Henry isn't at his side, and he turns back to see him hovering in the wings, just behind a curtain. Always hesitant to step on anyone's moment. That's not going to fly anymore. He's family. He's part of it all now, headlines and oil paintings and pages in the Library of Congress, etched right alongside. And he's part of them. Goddamn forever. “Come on!” Alex yells, waving him over, and Henry spares a second to look panicked before he's tipping his chin up and buttoning his suit jacket and stepping out onto the stage. He gravitates to Alex's side, beaming. Alex throws one arm around him and the other around June. Nora presses in at June's other side. And President Ellen Claremont steps up to the podium."`,
   },
 ];
